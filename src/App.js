1280c4dd00356dba3529e49bf0ef7fe83b364880
Morgane Kermer <kermer.morgane@gmail.com>
2021-02-02 13:33:50 +0100

    featured carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import UserLandingpage from './components/user-landingpage/UserLandingpage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/user-landingpage/UserLandingpage.js
+++ src/components/user-landingpage/UserLandingpage.js
@@ -1,8 +1,34 @@
 import react from 'react';
+import moment from 'moment';
+import { Jumbotron, Container, Row} from 'react-bootstrap';
+import Slideshow from './Slideshow'
+import './UserLandingpage.css';
 
 function UserLandingpage(){
+const date = moment().format("HH");
+    const daytime = (date) => {
+        if(date >= 5 && date <12){
+            return ("Good Morning ");
+        } else if(date >= 12 && date <17){
+            return ("Welcome back ");
+        } else if(date >= 17 || date <5){
+            return ("Good evening ");
+        };
+    };
+
     return(
         <div>
+            <Jumbotron fluid>
+                <h1>{daytime(date)} Username</h1>
+            </Jumbotron>
+
+
+            <Container className="carousel">
+                <Row>
+                        <Slideshow/>
+                </Row>
+            </Container>
+
             
         </div>
     )
