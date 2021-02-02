814ef0553a6adaf5960885c992c07722ec6d9601
Morgane Kermer <kermer.morgane@gmail.com>
2021-02-02 10:17:56 +0100

    created userlandingpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import UserLandingpage from './components/user-landingpage/UserLandingpage';
 
 function App() {
   return (
     <div className="App">
-      
+      <UserLandingpage/>
     </div>
   );
 }
---
+++ src/components/user-landingpage/UserLandingpage.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function UserLandingpage(){
+    return(
+        <div>
+            
+        </div>
+    )
+};
+
+export default UserLandingpage;
