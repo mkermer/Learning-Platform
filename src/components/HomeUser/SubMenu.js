7e46c05297a427b586d65e8ca39fc6a4169971f4
Yuliya Kalcheva <hulyakurt89@hotmail.com>
2021-02-08 09:29:09 +0100

    nav

--- src/components/HomeUser/SubMenu.js
+++ src/components/HomeUser/SubMenu.js
@@ -1,2 +1,53 @@
-import react from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SidebarLink= styled(Link)`
+display:flex;
+color:#e1e9fc;
+justify-content: space-between;
+align-items:center;
+padding:20px;
+list-style: none;
+height:60px;
+text.decoration:none;
+font-size:18px;
+
+&:hover {
+    background:#252831;
+    border-left:4px solid #632ce4;
+    cursor:pointer;
+}
+
+`; 
+const SidebarLabel =styled.span `
+margin-left:16px
+`;
+
+const SubMenu = ({ item }) => {
+const {subnav,setSubnav}= useState(false)
+
+const showSubnav = () =>setSubnav(!subnav)
+
+    const Submenu = ({ item }) =>
+    return (
+<>
+<SidebarLink to ={item.path} onclick= {item.subNav && showSubnav}>
+    <div>
+        {item.icon}
+        <SidebarLabel>{item.titel}</SidebarLabel>
+        </div>
+        <div>
+            {item.subNav && subnav
+             ? item.iconOpened 
+             : item. subNav 
+             ? item.iconClosed
+            : null}
+            </div>
+        <SidebarLink>
+            </>
+            );
+    
+
+};
+export default SubMenu;
