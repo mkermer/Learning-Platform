bd815416d6bc2e10a4d2ad0820fd1948475515a4
Morgane Kermer <kermer.morgane@gmail.com>
2021-02-09 16:51:44 +0100

    Rating DB

--- src/components/Instructor-Landingpage/ShowRating.js
+++ src/components/Instructor-Landingpage/ShowRating.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React ,{useEffect, useState} from 'react';
 import { Toast } from 'react-bootstrap';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import * as actions from '../../actions/app.action';
+import axios from 'axios';
+import config from '../../config/config';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../../actions/app.action';
+import ReactStars from 'react-rating-stars-component';
+import Moment from 'react-moment';
+
 
 function ShowRating(props){
+    const [reviewArray, setReviewArray] = useState([]);
+
+    useEffect(async() => {
+        setReviewArr();
+        console.log(reviewArray)
+    }, []) 
+
+    const setReviewArr = async () => {
+        const response = await axios.get(config.baseUrl + '/review');
+        console.log(response.data);
+        const reviews = response.data;
+        const instructorArr = [];
+        
+        reviews.map(review => {
+            if(review.instructor === props.applicationState.user.instructorName){
+                instructorArr.push(review);
+            }
+        })      
+
+        setReviewArray(instructorArr);
+        
+    }
+
+  
+
     return(
         <div className="ShowRating">
             <div className="heading"><h1> Latest Reviews</h1></div> 
+            {reviewArray.map(review => {
+                return(
+                    <Toast>
+            <Toast.Header>
+            <img src={review.image} className="rounded mr-2" alt="" />
+            <strong className="mr-auto">{review.student}</strong>
+            <ReactStars activeColor="blue" edit={false} size={20} count={5} isHalf={true} value={review.rating}/>
+
+
+            <small><Moment fromNow>{review.timestamp}</Moment></small>
+            </Toast.Header>
+            <Toast.Body>{review.text}</Toast.Body>
+        </Toast>
+                )
+            })}
         <Toast>
+            
             <Toast.Header>
             <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
             {/* <strong className="mr-auto">{props.applicationState.user.firstName}</strong> */}
@@ -52,4 +98,6 @@ function ShowRating(props){
     )
 }
 
-export default ShowRating;
+const mapStateToProps = state => ({ applicationState: state });
+const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(actions, dispatch) });
+export default connect(mapStateToProps, mapDispatchToProps)(ShowRating);
