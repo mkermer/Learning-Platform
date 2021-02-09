8ab1e2626c050b3fe1fb48ebb3e28d3afa350ee5
Morgane Kermer <kermer.morgane@gmail.com>
2021-02-09 15:10:39 +0100

    my videos

--- src/components/Instructor-Landingpage/MyVideos.js
+++ src/components/Instructor-Landingpage/MyVideos.js
@@ -1,29 +1,109 @@
-import React, {useState} from 'react';
-import { Button, Collapse } from 'react-bootstrap';
+import React, {useState, useContext} from 'react';
+import { Row, Col, Accordion, Card, Button} from 'react-bootstrap';
+import { Trash, ChevronBarUp } from 'react-bootstrap-icons';
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
+import { AccordionContext } from 'react-bootstrap';
 // import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 // import * as actions from '../../actions/app.action';
 
+function ContextAwareToggle({ children, eventKey, callback }) {
+    const currentEventKey = useContext(AccordionContext);
+  
+    const decoratedOnClick = useAccordionToggle(
+      eventKey,
+      () => callback && callback(eventKey),
+    );
+  
+    const isCurrentEventKey = currentEventKey === eventKey;
+  
+    return (
+      <ChevronBarUp  
+      size={25}
+      className={isCurrentEventKey ? "rotateUp" : "rotateDown"}
+      onClick={decoratedOnClick}
+      />
+    );
+  }
+
 function MyVideos(){
-    const [open, setOpen] = useState(true);
+    
 
     return (
     <div className="MyVideos">
         <h1>My Videos</h1>
-      <p
-        onClick={() => setOpen(!open)}
-        aria-controls="example-collapse-text"
-        aria-expanded={open}
-      >
-        CourseName
-      </p>
-      <Collapse in={open}>
-        <div id="example-collapse-text">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-          labore wes anderson cred nesciunt sapiente ea proident.
-        </div>
-      </Collapse>
+        
+        <Accordion defaultActiveKey="0">
+        <Card>
+            
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                <Row>
+                    <Col xs={10}>CourseName</Col>
+                    <Col xs={2}>
+                        <ContextAwareToggle as={Button} variant="link" eventKey="0"/>
+                    </Col>
+                </Row>
+                </Accordion.Toggle>
+            
+            <Accordion.Collapse eventKey="0">
+            <Card.Body>
+                <Row>
+                    <Col xs={10}>
+                        VideoName
+                    </Col>
+                    <Col xs={2}>
+                        <Trash className="Trash" size={20}/>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs={10}>
+                        VideoName
+                    </Col>
+                    <Col xs={2}>
+                        <Trash className="Trash" size={20}/>
+                    </Col>
+                </Row>
+            </Card.Body>
+            </Accordion.Collapse>
+        </Card>
+        </Accordion>
+        <Accordion defaultActiveKey="0">
+        <Card>
+            
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                <Row>
+                    <Col xs={10}>CourseName</Col>
+                    <Col xs={2}>
+                        <ContextAwareToggle as={Button} variant="link" eventKey="0"/>
+                    </Col>
+                </Row>
+                </Accordion.Toggle>
+            
+            <Accordion.Collapse eventKey="0">
+            <Card.Body>
+                <Row>
+                    <Col xs={10}>
+                        VideoName
+                    </Col>
+                    <Col xs={2}>
+                        <Trash className="Trash" size={20}/>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs={10}>
+                        VideoName
+                    </Col>
+                    <Col xs={2}>
+                        <Trash className="Trash" size={20}/>
+                    </Col>
+                </Row>
+            </Card.Body>
+            </Accordion.Collapse>
+        </Card>
+        </Accordion>
+ 
     </div>
   );
 }
