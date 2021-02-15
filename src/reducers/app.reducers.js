9bd8acbd744e20d9c3225c74ab36920e11b62b43
DavidWz0403 <52293898+DavidWz0403@users.noreply.github.com>
2021-02-15 18:58:43 +0100

    reset redux

--- src/reducers/app.reducers.js
+++ src/reducers/app.reducers.js
@@ -34,7 +34,9 @@ function appReducer(state = initialState, action) {
                 video: action.video
             }
         };
-
+        case "RESET_REDUX":
+            console.log('Reducer: RESET_REDUX')
+            return initialState; //Always return the initial state
         default:
             return state;
     }
